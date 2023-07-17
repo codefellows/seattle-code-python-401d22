2520ddd53e726da3a37dedb51a22d161d17f642f
Roger Huba <rogerhuba@gmail.com>
2023-07-17 10:28:16 -0500

    fix demp

--- class-26/demo/django-things/things/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import HomePageView, AboutView
-
-urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-path('about', AboutView.as_view(), name='about'),
-]
