1f513f1735d71cee54f222d532de357bb3f88e06
Roger Huba <rogerhuba@gmail.com>
2023-07-18 17:34:22 -0500

    fix demo

--- class-27/demo/thing-rater/things/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ThingListView, ThingDetailView
-
-urlpatterns = [
-    path('', ThingListView.as_view(), name="thing_list"),
-    path('<int:pk>/', ThingDetailView.as_view(), name="thing_detail"),
-]
