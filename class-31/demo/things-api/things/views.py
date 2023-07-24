8da03c3c5ff6c296273cb8c580fb57d914fb81e5
Roger Huba <rogerhuba@gmail.com>
2023-07-24 14:33:58 -0500

    add demo

---
+++ class-31/demo/things-api/things/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from .serializer import ThingSerializer
+from .models import Thing
+
+
+class ThingList(generics.ListCreateAPIView):
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
+
+
+class ThingDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
+
+
