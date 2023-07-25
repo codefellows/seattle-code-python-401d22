9d355adbae0119bca72556ed1c4d1c3a06269b20
Roger Huba <rogerhuba@gmail.com>
2023-07-25 11:03:26 -0500

    update

--- class-32/demo/things-api-permissions/things/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ThingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'things'
--- class-32/demo/things-api-postgres/things/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ThingList, ThingDetail
-
-urlpatterns = [
-    path("", ThingList.as_view(), name="thing_list"),
-    path("<int:pk>/", ThingDetail.as_view(), name="thing_detail"),
-]
