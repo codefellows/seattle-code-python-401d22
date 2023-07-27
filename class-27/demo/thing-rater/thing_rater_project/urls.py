d7d0ba41acc8fc9f1276a0d574f6178c43f47a42
Roger Huba <rogerhuba@gmail.com>
2023-07-27 12:01:23 -0500

    update

--- class-27/demo/thing-rater/thing_rater_project/urls.py
+++ class-27/demo/thing-rater/thing_rater_project/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('things.urls'))
+    path('', include('things.urls')),
+    path("accounts/", include("accounts.urls")),  # new
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
--- class-34/demo/snacks-api-again/snacks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SnackList, SnackDetail
-
-urlpatterns = [
-    path("", SnackList.as_view(), name="snack_list"),
-    path("<int:pk>/", SnackDetail.as_view(), name="snack_detail"),
-]
