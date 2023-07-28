2a0b75d9383b53308a666507b7f405752fa0b358
Roger Huba <rogerhuba@gmail.com>
2023-07-28 14:55:09 -0500

    add class 35 starter

---
+++ class-35/demo/graph.py
@@ -0,0 +1,70 @@
+class Graph:
+    """
+    Class
+    """
+    def __init__(self):
+        pass
+
+    def add_vertex(self):
+        """
+        Method: add vertex
+        Arguments: value
+        Add a vertex to the graph
+        :return: The added vertex
+        """
+        pass
+
+    def add_edge(self):
+        """
+        Method: add edge
+        Arguments: 2 vertices to be connected by the edge, weight (optional)
+        Adds a new edge between two vertices in the graph
+        If specified, assign a weight to the edge
+        Both vertices should already be in the Graph
+        :return: nothing
+        """
+        pass
+
+    def get_vertices(self):
+        """
+        Method: get vertices
+        Arguments: none
+        Empty collection returned if there are no vertices
+        :return: all the vertices in the graph as a collection (set, list, or similar)
+        """
+        pass
+
+    def get_neighbors(self):
+        """
+        Method: get neighbors
+        Arguments: vertex
+        Include the weight of the connection in the returned collection
+        Empty collection returned if there are no vertices
+        :return: a collection of edges connected to the given vertex
+        """
+        pass
+
+    def size(self):
+        """
+        Method: size
+        Arguments: none
+        0 if there are none
+        :return: the total number of vertices in the graph
+        """
+        pass
+
+
+class Vertex:
+    """
+    Class
+    """
+    def __init__(self):
+        pass
+
+
+class Edge:
+    """
+    Class
+    """
+    def __init__(self):
+        pass
