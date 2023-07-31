ba61e97f9041a0757ee451cc345a21fdcf25d77c
Roger Huba <rogerhuba@gmail.com>
2023-07-31 14:22:49 -0500

    add demo

--- class-36/demo/demo.py
+++ class-36/demo/demo.py
@@ -11,3 +11,48 @@ def is_panagram(word):
         return True
     else:
         return False
+
+
+def delete_over(lst, num):
+    # Iterate through the list of LinkedLists one by one
+    for ll in lst:
+        # Set the 'current' variable to the head of the current LinkedList
+        current = ll.head
+        # Initialize a 'prev' variable to keep track of the previous node in the LinkedList
+        prev = None
+        # Traverse the current LinkedList
+        while current:
+            # Check if the value of the current node is greater than 'num'
+            if current.vaue > num:
+                # If the current node is the head of the LinkedList
+                if current == ll.head:
+                    # Update the head to the next node (remove the current node)
+                    ll.head = current.next
+                else:
+                    prev.next = current.next
+
+            else:
+                # If the current node is not the head
+                # Update the 'next' of the previous node to skip the current node
+                # Move to the next node in the LinkedList
+                prev = current
+            current = current.next
+
+
+
+
+# Walkthrough
+
+# lst = (ll1, ll2)
+#              ^
+# Int : 9
+
+# ll1 = [10] -> None
+#                ^
+# current:
+# previous:
+
+# ll2 = [3] -> [10] -> [5] -> None
+#               ^
+# current:
+# previous:
