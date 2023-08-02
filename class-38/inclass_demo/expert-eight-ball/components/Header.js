862f0481d691f48f9b7866cc842c469a18115788
Adam Owada <adamowada@gmail.com>
2023-08-02 11:29:23 -0700

    Added question counter to header component.

--- class-38/inclass_demo/expert-eight-ball/components/Header.js
+++ class-38/inclass_demo/expert-eight-ball/components/Header.js
@@ -2,6 +2,7 @@ export default function Header() {
     return (
         <header className="flex items-center justify-between p-4 bg-gray-500 text-gray-50">
             <h1 className="text-4xl">Expert 8 Ball</h1>
+            <p>0 questions answered</p>
         </header>
     );
 }
