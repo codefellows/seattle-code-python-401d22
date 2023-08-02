3cddb57fa6aacac0774e0df10b2751e30064b719
Adam Owada <adamowada@gmail.com>
2023-08-02 11:33:31 -0700

    Fix answer count display in Header component to show the correct number of questions answered.

--- class-38/inclass_demo/expert-eight-ball/components/Header.js
+++ class-38/inclass_demo/expert-eight-ball/components/Header.js
@@ -2,7 +2,7 @@ export default function Header({ answerCount }) {
     return (
         <header className="flex items-center justify-between p-4 bg-gray-500 text-gray-50">
             <h1 className="text-4xl">Expert 8 Ball</h1>
-            <p>answerCount questions answered</p>
+            <p>{answerCount} questions answered</p>
         </header>
     );
 }
