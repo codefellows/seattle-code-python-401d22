f092dab6e0e15cf2034a84e3698163b326961e8b
Adam Owada <adamowada@gmail.com>
2023-08-02 11:37:41 -0700

    Add RepliesTable component and update variable name in Home component.

--- class-38/inclass_demo/expert-eight-ball/pages/index.js
+++ class-38/inclass_demo/expert-eight-ball/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
                 {/* Eight Ball */}
                 <EightBall question={question} />
 
-                {/* Replies(Table) */}
+                {/* RepliesTable */}
                 {/* Conditional rendering */}
                 {magicReplies.length > 0 ? (
                     <table className="w-1/2 mx-auto my-4 border">
