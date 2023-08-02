e57049cc3c4cac1d07dd8fa6485b59e1ed53cb2d
Adam Owada <adamowada@gmail.com>
2023-08-02 10:24:35 -0700

    Added Header component to expert-eight-ball app.

---
+++ class-38/inclass_demo/expert-eight-ball/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex items-center justify-between p-4 bg-gray-500 text-gray-50">
+            <h1 className="text-4xl">Expert 8 Ball</h1>
+        </header>
+    );
+}
