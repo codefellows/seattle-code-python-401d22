a55d62059d1d59a41d4be7b329c4ace3767a227a
Adam Owada <adamowada@gmail.com>
2023-08-02 11:48:59 -0700

    Refactored Careers page with link to return home.

--- class-38/inclass_demo/expert-eight-ball/pages/careers.js
+++ class-38/inclass_demo/expert-eight-ball/pages/careers.js
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Careers() {
     return (
-        <div>
-            <h1>Careers Page</h1>
-            <p>Coming Soon</p>
+        <div className="flex items-center justify-center mx-auto my-8 bg-gray-500 w-72 h-72 text-gray-50">
+            <section className="space-y-4">
+                <h1 className="text-4xl">Careers Page</h1>
+                <Link href="/">
+                    <a className="block text-center underline">Return Home</a>
+                </Link>
+            </section>
         </div>
     );
 }
