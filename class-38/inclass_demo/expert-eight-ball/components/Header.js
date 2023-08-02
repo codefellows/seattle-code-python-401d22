b7cab489d25fbc0d0fa916eb8cd356775075ad70
Adam Owada <adamowada@gmail.com>
2023-08-02 11:31:26 -0700

    Refactored Header component to display the number of questions answered and passed answerCount prop from Home component.

--- class-38/inclass_demo/expert-eight-ball/components/Header.js
+++ class-38/inclass_demo/expert-eight-ball/components/Header.js
@@ -1,8 +1,8 @@
-export default function Header() {
+export default function Header({ answerCount }) {
     return (
         <header className="flex items-center justify-between p-4 bg-gray-500 text-gray-50">
             <h1 className="text-4xl">Expert 8 Ball</h1>
-            <p>0 questions answered</p>
+            <p>{answerCount} questions answered</p>
         </header>
     );
 }
--- class-38/inclass_demo/expert-eight-ball/pages/index.js
+++ class-38/inclass_demo/expert-eight-ball/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
             </Head>
 
             {/* Header */}
-            <Header />
+            <Header answerCount={magicReplies.length} />
 
             <main className="flex flex-col items-center py-4 space-y-8">
                 {/* QuestionForm */}
