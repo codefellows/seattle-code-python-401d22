744186aab9eb1179452177ed02b5661bae74a160
Adam Owada <adamowada@gmail.com>
2023-08-02 10:38:59 -0700

    Added QuestionForm component and replaced form tag with QuestionForm component in Home page.

---
+++ class-38/inclass_demo/expert-eight-ball/components/QuestionForm.js
@@ -0,0 +1,11 @@
+export default function QuestionForm() {
+    return (
+        <form
+            className="flex w-1/2 p-2 my-4 bg-gray-200 mx-auto"
+            onSubmit={questionAskedHandler}
+        >
+            <input name="question" className="flex-auto pl-1" />
+            <button className="px-1 py-1 bg-gray-400 text-gray-50">Ask</button>
+        </form>
+    );
+}
--- class-38/inclass_demo/expert-eight-ball/pages/index.js
+++ class-38/inclass_demo/expert-eight-ball/pages/index.js
@@ -32,10 +32,7 @@ export default function Home() {
             <main className="flex flex-col items-center py-4 space-y-8">
 
                 {/* QuestionForm */}
-                <form className="flex w-1/2 p-2 my-4 bg-gray-200 mx-auto" onSubmit={questionAskedHandler}>
-                    <input name="question" className="flex-auto pl-1"/>
-                    <button className="px-1 py-1 bg-gray-400 text-gray-50">Ask</button>
-                </form>
+                <QuestionForm />
 
                 {/* Eight Ball */}
                 <EightBall question={question} />
