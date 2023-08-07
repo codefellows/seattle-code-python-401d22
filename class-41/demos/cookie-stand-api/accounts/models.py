4b374b3fe5b56aa56e46a3866078e23e1fda9877
Roger Huba <rogerhuba@gmail.com>
2023-08-07 08:52:48 -0500

    update

--- class-41/demos/cookie-stand-api/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-
-
-class CustomUser(AbstractUser):
-    pass
-    # add additional fields in here
-
-    def __str__(self):
-        return self.username
